39fe8f2644ac4476afd338c10aa34517df76a58a
Awais Saeed <awaissaeed530@gmail.com>
2024-01-10 00:57:07 +0500

    fix: correct color shade for admin welcome message

--- libs/admin/shell/src/lib/shell.component.ts
+++ libs/admin/shell/src/lib/shell.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-shell',
-  template: `<div class="text-green-500">Admin Shell</div>`,
+  template: `<div class="text-green-600">Admin Shell</div>`,
 })
 export class AdminShellComponent {}
