88a17ec8f7c796a76011b45e60b8378f3f3a1789
Awais Saeed <awaissaeed530@gmail.com>
2024-01-10 01:21:56 +0500

    feat(admin-shell): create state machine for absolutely no reason

--- libs/admin/shell/src/lib/shell.component.ts
+++ libs/admin/shell/src/lib/shell.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { createStateMachine } from '@workspace/shared/state-machine';
 
 @Component({
   selector: 'app-admin-shell',
   template: `<div class="text-green-600">Admin Shell</div>`,
 })
-export class AdminShellComponent {}
+export class AdminShellComponent {
+  constructor() {
+    const message = createStateMachine();
+    console.log(message);
+  }
+}
